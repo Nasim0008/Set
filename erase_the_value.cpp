0706cd2ddde4736848c5b1c0b558f07c880624fe
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-12 00:20:28 +0600

    erase any value from the set

---
+++ erase_the_value.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    set<int>s;
+    s.insert(5);
+    s.insert(2);
+    s.insert(5);
+    s.insert(1);
+    s.insert(9);
+    s.insert(5);
+    s.insert(4);
+
+
+    s.erase(5);
+
+    cout<<s.size()<<endl;
+//    set<int>::iterator it;
+//    for(it=s.begin();it!=s.end();it++)
+//    {
+//        cout<< *it<< " ";
+//    }
+//    cout<<endl;
+
+    for(auto u:s)
+    {
+        cout<<u<< " ";
+    }
+    cout<<endl;
+
+    s.clear();
+    if(s.empty())
+    {
+        cout<< "Empty"<<endl;
+    }
+    else
+    {
+        cout<< "Not Empty"<<endl;
+    }
+
+}
+
