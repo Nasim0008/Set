1ed1446de673e9fcf9ab298a14d24d2281392964
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-14 17:38:23 +0600

    clear all the elements from the multiset

---
+++ Multiset/clear.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset<int,greater<int>>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(5);
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(4);
+    ms.insert(4);
+
+    ms.clear();
+    cout<<ms.size()<<endl;
+}
