7684ef882aece37d924a3b349770f2574dc40b32
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-14 17:59:22 +0600

    lower_bound.cpp

---
+++ Multiset/lower_bound.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset<int>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(5);
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(4);
+    ms.insert(4);
+     
+    auto low = ms.lower_bound(2);
+    cout<< *low<<endl;
+    
+}
