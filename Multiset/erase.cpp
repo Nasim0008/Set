eb10bac3dffbc2142e103a948de3b6c283ed7ed4
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-14 17:43:39 +0600

    erase any specific elelments

---
+++ Multiset/erase.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset<int>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(5);
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(4);
+    ms.insert(4);
+
+    for(auto u:ms)
+    {
+        cout<<u<< " ";
+    }
+    cout<<endl;
+
+    ms.erase(2);
+    for(auto u:ms)
+    {
+        cout<< u<< " ";
+    }
+    cout<<endl;
+}
