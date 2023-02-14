022c82559c9984c46a2500f7f4bb16ae75af43bd
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-14 15:53:04 +0600

    print the multiset  in ascending order

---
+++ Multiset/print_ascending.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset<int>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(5);
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(4);
+    ms.insert(4);
+
+    for(auto u:ms)
+    {
+        cout<<u<< " ";
+    }
+    cout<<endl;
+
+}
