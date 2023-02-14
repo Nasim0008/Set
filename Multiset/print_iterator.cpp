918ce57a0fafc94ddbd745eb4e86eba2bf095db3
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-02-14 16:01:38 +0600

    print the value by using multiset

---
+++ Multiset/print_iterator.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    multiset<int>ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(5);
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(4);
+    ms.insert(4);
+
+    int sz = ms.size();
+    cout<< sz<<endl;
+
+    multiset<int>::iterator it;
+    for(it = ms.begin();it!=ms.end();it++)
+    {
+        cout<<*it<< " ";
+    }
+    cout<<endl;
+
+   
+
+}
