01df027ef415d75f1761491974c9878d40a3128d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-08-09 15:56:02 +0600

    lower bound of the set

--- lower_bound.cpp
+++ lower_bound.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 int main()
 {
-    set<int>s={1,3,4,2,5,6};
-    for(auto u:s)
+    //set declaration
+    set<int>se;
+
+    // insert the new value in the set
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
     {
-        cout<<u<< " ";
+        int value;
+        cin>>value;
+        se.insert(value);
     }
-    cout<<endl;
-    set<int>:: iterator it;
-    it=s.lower_bound(3);
-    if(it!=s.end())
-    {
-        cout<< "lower Bound of 3 is "<<*it<<endl;
-    }
-    else
-    {
-        cout<< "lower bound is the larger than the greater elements"<<endl;
-    }
-}
+    int target;
+    cin>>target;
+    auto u = se.lower_bound(target);
+    /* 
+    lower bound situation occurs in the lower bound
+    1. if the value is present then it that value
+    2. if the value is not present then it give imiditate bigger value 
+    3. if the value is greater than the max element of the set then it give the max element 
+    */
+    cout<<*u<<endl;
+
+}  
