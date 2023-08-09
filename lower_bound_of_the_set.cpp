b9304aea9cbd68553545776f73f86a488c124b71
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-08-09 15:59:22 +0600

    find the lower bound of the set

---
+++ lower_bound_of_the_set.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //set declaration
+    set<int>se;
+
+    // insert the new value in the set
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int value;
+        cin>>value;
+        se.insert(value);
+    }
+    int target;
+    cin>>target;
+    auto u = se.lower_bound(target);
+    /* 
+    lower bound situation occurs in the lower bound
+    1. if the value is present then it that value
+    2. if the value is not present then it give imiditate bigger value 
+    3. if the value is greater than the max element of the set then it give the max element 
+    */
+    cout<<*u<<endl;
+
+}  
